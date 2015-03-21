04175b990b012a1a12db80803e5d9c23fa08dd0d
AjayRajanala <ajay.rajanala01@gmail.com>
2015-03-21 12:45:11 +0530

    Final Commit
    Commited on Sat

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below 2 functions make use of the scoping rules of R to cache the long taking computations and store it in the local environment of the instance of the function.
+## The value once computed for a matrix can be fetched multiple times from the cache
 
-## Write a short comment describing this function
+## This function gives a list of the functions to be used to set and get the values and means of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 		minv <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                minv <<- NULL
         }
         get <- function() x
         setinv <- function(inv) minv <<- inv
-        getinv <- function() m
+        getinv <- function() minv
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function created a function that reuses the value in cache if found else computes the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
